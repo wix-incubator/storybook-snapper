38121de43c2e09897ef843007b7d3901c31a64d8
Shlomi Toussia-Cohen <shlomitc@wix.com>
2021-05-12 00:02:18 +0300

    docs

--- config/gitData.js
+++ config/gitData.js
@@ -1,8 +1,8 @@
 const { execSync } = require('child_process');
 
 function getGitData () {
-  const [, orgNameAndProjectName] = execSync('git config --get remote.origin.url').toString().split(':');
-  const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().replace('\n', '');
+  const [, orgNameAndProjectName] = execSync('git config --get remote.origin.url').toString().split(':'); // e.g. git@github.com:wix-incubator/storybook-snapper.git
+  const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().replace('\n', ''); // e.g. myBranch\n
 
   const [orgName, projectNameWithSuffix] = orgNameAndProjectName.split('/');
   const [projectName] = projectNameWithSuffix.split('.git');
@@ -13,6 +13,5 @@ function getGitData () {
     branchName
   }
 }
-
 
 module.exports = { getGitData };
